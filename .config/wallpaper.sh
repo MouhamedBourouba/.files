a34f9b508d65492ae660f34dbc73c45df2649acb
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2024-10-19 20:12:17 +0100

    fixed

--- .config/wallpaper.sh
+++ .config/wallpaper.sh
@@ -2,30 +2,29 @@
 
 COUNT_FILE="lastWallpaper"
 W_DIR="/home/mouhamed/Pictures/Wallpapers"
-LAST_INDEX=0;
 
 if [ ! -f $COUNT_FILE ]; then
   echo "0" > $COUNT_FILE
 fi
 
-wallpaperIndex=$(cat $COUNT_FILE)
-numberOfWallpapers=$(ls $W_DIR | wc -w)
+WALLPAPER_INDEX=$(cat $COUNT_FILE)
+NUMBER_OF_WALLPAPERS=$(ls $W_DIR | wc -w)
 
-if [ $wallpaperIndex -ge $numberOfWallpapers ]; then
+if [ $WALLPAPER_INDEX -ge $NUMBER_OF_WALLPAPERS ]; then
   echo "0" > $COUNT_FILE
-  wallpaperIndex=0;
+  WALLPAPER_INDEX=0;
 fi
 
 i=0
-wallpaperFile=""
+WALLPAPER_FILE=""
 
 for file in $(ls $W_DIR); do
-  if [ $wallpaperIndex -eq $i ]; then wallpaperFile=$file; fi
+  if [ $WALLPAPER_INDEX -eq $i ]; then WALLPAPER_FILE=$file; fi
   i=$((i + 1))
 done
 
-fullpath="$W_DIR"'/'"$wallpaperFile"
-feh --bg-fill $fullpath
+FULLPATH="$W_DIR"'/'"$WALLPAPER_FILE"
+feh --bg-fill $FULLPATH
 
-wallpaperIndex=$((wallpaperIndex + 1))
-echo $wallpaperIndex > $COUNT_FILE;
+WALLPAPER_INDEX=$((WALLPAPER_INDEX + 1))
+echo $WALLPAPER_INDEX > $COUNT_FILE;
