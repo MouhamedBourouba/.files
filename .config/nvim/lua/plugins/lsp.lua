67ddabbf4ae949a0e217cfbcdf886cb4a2b9617e
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-02-01 14:29:14 +0100

    fixed snippet

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -18,10 +18,13 @@ return {
           local ls = require("luasnip")
           local s = ls.snippet
           local t = ls.text_node
+          local i = ls.insert_node
 
           ls.add_snippets("javascript", {
             s("jd", {
-              t({ "/**", " * ", " */" }),
+              t({ "/**", " * " }),
+              i(1),
+              t({ "", " */" }),
             }),
           })
         end,
