32695085e2496932b9a8d945e992e4379480c5e8
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-03-03 19:06:27 +0100

    treesitter disabled for large files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -20,6 +20,13 @@ return {
       },
       highlight = {
         enable = true,
+        disable = function(_, buf)
+          local max_filesize = 70 * 1024
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
       },
       auto_install = true,
     })
