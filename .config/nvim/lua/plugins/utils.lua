bc0ba6bf52fcc189556ec6760d74b1a803000f54
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2025-07-31 20:59:57 +0100

    undotree

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -3,10 +3,12 @@ vim.pack.add {
   "https://github.com/folke/flash.nvim",
   "https://github.com/tpope/vim-fugitive",
   "https://github.com/folke/zen-mode.nvim",
+  "https://github.com/mbbill/undotree",
 }
 
 vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end)
 vim.keymap.set("n", "<C-g>", ":Git ")
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 require("zen-mode").setup({})
 
